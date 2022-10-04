146bcf699cf19824987259e481ae55dd47b7728a
Nathaniel Truitt <ntruitt@Nathaniels-MacBook-Pro.local>
2022-10-04 11:15:51 -0500

    adding an EventEmitter to navigation

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnInit {
+  @Output() currentPage = new EventEmitter();
   collapsed = true;
   show = false;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelectPage(page: string) {
+    // emit the currentPage string up to app component which then stores it in the selectedFeature property.
+    // * refer to line 2 on app.component.html
+    this.currentPage.emit(page);
+  }
 }
