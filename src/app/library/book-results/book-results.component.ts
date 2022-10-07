347488cdaa8a01b4c4968747438b22663e9f3325
Nathaniel Truitt <ntruitt@Nathaniels-MacBook-Pro.local>
2022-10-07 07:44:32 -0500

    adding app-results code and fixing issue in book-results

--- src/app/library/book-results/book-results.component.ts
+++ src/app/library/book-results/book-results.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { BookshelfService } from 'src/app/bookshelf/bookshelf.service';
+import { Book } from 'src/app/shared/book/book.model';
+import { LibraryService } from '../library.service';
 
 @Component({
   selector: 'app-book-results',
   templateUrl: './book-results.component.html',
-  styleUrls: ['./book-results.component.css']
+  styleUrls: ['./book-results.component.css'],
 })
 export class BookResultsComponent implements OnInit {
+  constructor(
+    private libraryService: LibraryService,
+    private bookshelfService: BookshelfService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSaveBook(book: Book) {
+    return this.bookshelfService.saveBook(book);
   }
 
+  onGetBooks() {
+    return this.libraryService.bookList$;
+  }
 }
--- src/app/library/library.service.ts
+++ src/app/library/library.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Book } from '../shared/book/book.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LibraryService {
+  private allBooks: Book[] = [
+    new Book(
+      'API Book 1',
+      'Will Wilder',
+      'Mystery',
+      'https://source.unsplash.com/50x50/?mystery,book'
+    ),
+    new Book(
+      'API Book 2',
+      'Nolan Hovis',
+      'Non-Fiction',
+      'https://source.unsplash.com/50x50/?serious,book'
+    ),
+    new Book(
+      'API Book 3',
+      'German Cruz',
+      'Mystery',
+      'https://source.unsplash.com/50x50/?mystery,book'
+    ),
+    new Book(
+      'API Book 4',
+      'Lex Pryor',
+      'Non-Fiction',
+      'https://source.unsplash.com/50x50/?serious,book'
+    ),
+  ];
+  bookList$ = new BehaviorSubject<Book[]>(this.allBooks.slice());
 
-  constructor() { }
+  constructor() {}
 }
